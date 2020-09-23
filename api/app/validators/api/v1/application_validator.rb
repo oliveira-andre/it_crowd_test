e728d59dfdc8cef96c0295e08079763f6e2af496
Andre Oliveira <desmonplays@gmail.com>
2020-09-22 23:20:13 -0300

    feat: loading params, fetching and validating person

---
+++ api/app/validators/api/v1/application_validator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ApplicationValidator
+      attr_accessor :params
+
+      def initialize(params)
+        @params = params
+      end
+    end
+  end
+end
---
+++ api/app/validators/api/v1/people/create.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module People
+      class Create < ApplicationValidator
+        def fetch_and_validate
+          Person.create!(params)
+        rescue ActiveRecord::RecordInvalid
+          raise ServiceException.new, I18n.t('errors.invalid_params')
+        end
+      end
+    end
+  end
+end
