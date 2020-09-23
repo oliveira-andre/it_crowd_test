ce2ef7ecfdaff0b4431df394a21a55126e267a1f
Andre Oliveira <desmonplays@gmail.com>
2020-09-22 22:20:03 -0300

    feat: validating fields, hashing password with bcrypt, and adding the methods movies

---
+++ api/app/models/person.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Person < ApplicationRecord
+  include BCrypt
+
+  attr_reader :password
+  has_secure_password
+
+  validates_presence_of :first_name, :last_name, :email, :password_digest
+  validates_uniqueness_of :email
+  validates :email, format: { with: EMAIL_VALIDATOR }
+  validates :password, length: { minimum: 6 }
+
+  has_many :movie_people
+  has_many :movies, through: :movie_people
+
+  def password=(value)
+    self.password_digest = Password.create(value)
+    @password = value
+  end
+
+  def movies_as_actors
+    movies.joins(:movie_people).where(movie_people: { role: :actors })
+  end
+
+  def movies_as_director
+    movies.joins(:movie_people).where(movie_people: { role: :director })
+  end
+
+  def movies_as_producer
+    movies.joins(:movie_people).where(movie_people: { role: :producer })
+  end
+end
