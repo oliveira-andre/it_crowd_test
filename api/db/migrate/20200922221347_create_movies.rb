f54b07c6a6b1831796adb62b35e0a3b48e52d9c7
Andre Oliveira <desmonplays@gmail.com>
2020-09-23 01:46:06 -0300

    feat: adding column released_at to movies table

--- api/db/migrate/20200922221347_create_movies.rb
+++ api/db/migrate/20200922221347_create_movies.rb
@@ -2,6 +2,7 @@ class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
       t.string :title, null: false
+      t.date :released_at, null: false
 
       t.timestamps
     end
