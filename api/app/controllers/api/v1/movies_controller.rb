3d1279d9ba40fc568e6412b6021c6b83399a5d0b
Andre Oliveira <desmonplays@gmail.com>
2020-09-23 17:12:56 -0300

    feat: sending params to validator movie update

--- api/app/controllers/api/v1/movies_controller.rb
+++ api/app/controllers/api/v1/movies_controller.rb
@@ -9,12 +9,22 @@ module Api
         serializer(Movies::Create.new(create_movie_params).fetch_and_validate)
       end
 
+      def update
+        serializer(Movies::Update.new(update_movie_params).fetch_and_validate)
+      end
+
       private
 
       def create_movie_params
         params.require(:movie).permit(
           :title, :released_at, movie_people_attributes: %i[role person_id]
         )
+      end
+
+      def update_movie_params
+        params.require(:movie).permit(
+          :title, :released_at, movie_people_attributes: %i[id role person_id]
+        ).merge(movie_id: params[:id])
       end
     end
   end
