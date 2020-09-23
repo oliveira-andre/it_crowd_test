a1322c5612eb8334d03316285847b7d18c215ff1
Andre Oliveira <desmonplays@gmail.com>
2020-09-22 23:21:14 -0300

    feat: rescuing the service exception with serializer error

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ServiceException do |e|
+    error_serializer(e.message)
+  end
+
   def serializer(data, serializer, status: 201)
     render json: "Api::V1::#{serializer}".constantize.new(data).serialize,
            status: status
+  end
+
+  def error_serializer(error_message)
+    render json: { errors: error_message }, status: 422
   end
 end
