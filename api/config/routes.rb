e6765b3549f73f1c43b70df4a834f763c89df891
Andre Oliveira <desmonplays@gmail.com>
2020-09-23 18:58:13 -0300

    feat: resourcing movies index

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :sessions, only: :create
       resources :people, only: %i[index create]
-      resources :movies, only: %i[create update destroy show]
+      resources :movies, except: %i[new edit]
     end
   end
 end
