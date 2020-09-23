55f1bf323306834e6932510ee2b977e27b76a6e9
Andre Oliveira <desmonplays@gmail.com>
2020-09-23 19:09:11 -0300

    fix: removing unnecessary index on people

--- api/app/controllers/api/v1/people_controller.rb
+++ api/app/controllers/api/v1/people_controller.rb
@@ -3,32 +3,28 @@
 module Api
   module V1
     class PeopleController < ApplicationController
-      before_action :load_people
-
-      def index
-        serializer(index_people_params)
-      end
+      before_action :authenticate_person!, except: :create
 
       def create
-        serializer(
-          People::Create.new(create_person_params).fetch_and_validate
-        )
+        serializer(People::Create.new(create_person_params).fetch_and_validate)
+      end
+
+      def update
+        serializer(People::Create.new(update_person_params).fetch_and_validate)
       end
 
       private
-
-      def load_people
-        @people = Person.all
-      end
-
-      def index_people_params
-        { data: @people, serializer: PeopleSerializer, status: 200 }
-      end
 
       def create_person_params
         params.require(:person).permit(
           :first_name, :last_name, :email, :aliases
         ).merge(password: params[:password])
+      end
+
+      def update_people_params
+        params.require(:person).permit(
+          :first_name, :last_name, :aliases
+        )
       end
     end
   end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :sessions, only: :create
-      resources :people, only: %i[index create]
+      resources :people, only: %i[create update]
       resources :movies, except: %i[new edit]
     end
   end
