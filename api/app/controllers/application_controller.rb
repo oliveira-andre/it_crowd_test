f33762e05d1b6d67475bd75d563182958ca241df
Andre Oliveira <desmonplays@gmail.com>
2020-09-22 23:08:37 -0300

    feat: serialize load data from controllers

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def serializer(data, serializer, status: 201)
+    render json: "Api::V1::#{serializer}".constantize.new(data).serialize,
+           status: status
+  end
 end
