59958d8b1f7fa40105a1d4b5c1c603573b69384c
Andre Oliveira <desmonplays@gmail.com>
2020-09-23 13:21:42 -0300

    feat: resoucing movies only create

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :sessions, only: :create
       resources :people, only: %i[index create]
+      resources :movies, only: :create
     end
   end
 end
