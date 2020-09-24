3b39d7c29ea7536807723e527abc7ac5cc3551c0
Andre Oliveira <desmonplays@gmail.com>
2020-09-24 01:10:04 -0300

    spec: adding all traits to person factory

--- api/spec/factories/person.rb
+++ api/spec/factories/person.rb
@@ -4,15 +4,29 @@ FactoryBot.define do
   factory :person do
     first_name { FFaker::Internet.user_name }
     last_name { FFaker::Internet.user_name }
-    email { FFaker::Internet.disposable_email }
+    email { "#{FFaker::Internet.domain_word}@gmail.com" }
     password { FFaker::Internet.password }
 
     trait :actors do
-      after(:create) do |movie|
-        create(:movie_person_actors, movie: movie)
+      after(:create) do |person|
+        create(:movie_person_actors, movie: person)
+      end
+    end
+
+    trait :director do
+      after(:create) do |person|
+        create(:movie_person_director, movie: person)
+      end
+    end
+
+    trait :producer do
+      after(:create) do |person|
+        create(:movie_person_producer, movie: person)
       end
     end
 
     factory :movie_as_actors, traits: %i[actors]
+    factory :movie_as_director, traits: %i[director]
+    factory :movie_as_producer, traits: %i[producer]
   end
 end
